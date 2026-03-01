bf3aea40dc94f9fcdd4f031394da3990165a56d5
Arjav <arjavbadjate1709@gmail.com>
2026-03-01 14:49:22 +0530

    Use prisma.js only with type declarations

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,10 +1,8 @@
-import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
-
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export { prisma };
